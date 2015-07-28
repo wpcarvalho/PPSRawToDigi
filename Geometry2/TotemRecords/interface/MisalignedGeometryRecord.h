eeaef3b2b90135239548f34e61eb42fd0ff97569
Tomasz Lichon <tomasz.jan.lichon@cern.ch>
2015-07-28 17:27:31 +0300

    Add Totem related files to 7_5_0 geometry.

---
+++ Geometry2/TotemRecords/interface/MisalignedGeometryRecord.h
@@ -0,0 +1,34 @@
+/****************************************************************************
+*
+* This is a part of TOTEM offline software.
+* Authors: 
+*	Jan Kaspar (jan.kaspar@gmail.com) 
+*    
+* $$RCSfile: MisalignedGeometryRecord.h,v $: $
+* $Revision: 9977 $
+* $Date: 2015-01-12 16:00:26 +0200 (pon, 12 sty 2015) $
+*
+****************************************************************************/
+
+#ifndef RECORDS_MISALIGNEDGEOMETRYRECORD_H
+#define RECORDS_MISALIGNEDGEOMETRYRECORD_H
+
+#include "FWCore/Framework/interface/DependentRecordImplementation.h"
+#include "Geometry/TotemRecords/interface/MeasuredGeometryRecord.h"
+
+#include "boost/mpl/vector.hpp"
+
+#include "TotemAlignment/RPRecords/interface/RPMisalignedAlignmentRecord.h"
+
+/**
+ * \ingroup TotemRPGeometry
+ * \brief Event setup record containing the misaligned geometry information. It is used for 
+ * alignment studies only.
+ **/
+class MisalignedGeometryRecord : public edm::eventsetup::DependentRecordImplementation
+						   <MisalignedGeometryRecord, boost::mpl::vector<MeasuredGeometryRecord, RPMisalignedAlignmentRecord /*, ... */> >
+{
+};
+
+#endif
+
