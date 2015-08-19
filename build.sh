1e5db884d0a8b8a473a76b573192b9161d260db4
Krzysztof Trzepla <krzysztof.andrzej.trzepla@cern.ch>
2015-08-19 14:38:48 +0200

    Change Jenkins build script, so that everything happens in the plan workspace.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 AGENT_USERNAME=`whoami`
-AGENT_KERBEROS_KEYTAB="/etc/${AGENT_USERNAME}.keytab"
+AGENT_KERBEROS_KEYTAB="/etc/$AGENT_USERNAME.keytab"
 AGENT_EXECUTORS=4
-AGENT_BUILD_LOG="${WORKSPACE}/build.log"
+AGENT_BUILD_LOG="$WORKSPACE/build.log"
 
 set -e # Exit immediately if a simple command exits with a non-zero status.
 set -x # Log all commands to stdout.
@@ -22,8 +22,8 @@ function show_current_directory_details() {
 
 # Initializes Kerberos keytab for agent user.
 function initialize_kerberos_keytab() {
-	echo "Initializing Kerberos keytab for user '${AGENT_USERNAME}'..."
-	kinit -kt ${AGENT_KERBEROS_KEYTAB} ${AGENT_USERNAME}
+	echo "Initializing Kerberos keytab for user '$AGENT_USERNAME'..."
+	kinit -kt "$AGENT_KERBEROS_KEYTAB" "$AGENT_USERNAME"
 }
 
 # Initializes scram project.
@@ -32,11 +32,9 @@ function initialize_kerberos_keytab() {
 function initialize_scram_project() {
 	echo "Initializing scram project..."
 	show_current_directory_details
-	cd ..
+	scram project -n "$JOB_NAME" "$1" "$2"
 	show_current_directory_details
-	scram project -n ${JOB_NAME} $1 $2
-	show_current_directory_details
-	cd ${JOB_NAME}
+	cd "$JOB_NAME"
 	show_current_directory_details
 	cmsenv
 }
@@ -46,10 +44,10 @@ function compile_scram_project() {
 	echo "Compiling scram project..."
 	cd src
 	show_current_directory_details
-	echo "Starting parallel compilation using up to ${AGENT_EXECUTORS} threads..."
-	scram build -j${AGENT_EXECUTORS} || \
-	scram build -j${AGENT_EXECUTORS} || \
-	echo "Parallel compilation using up to ${AGENT_EXECUTORS} threads failed..."
+	echo "Starting parallel compilation using up to $AGENT_EXECUTORS threads..."
+	scram build -j"$AGENT_EXECUTORS" || \
+	scram build -j"$AGENT_EXECUTORS" || \
+	echo "Parallel compilation using up to $AGENT_EXECUTORS threads failed..."
 	echo "Starting sequential compilation..."
 	scram build
 }
@@ -60,7 +58,7 @@ function maybe_build_merging_software() {
 	if [ "XbuildMergingSoftware" = "X$1" ]; then
 		echo "Building merging software..."
   		cd MergingSoftware/MergeCMSTOTEMNTuples/Merge/
-  		make -j${AGENT_EXECUTORS}
+  		make -j"$AGENT_EXECUTORS"
 	fi
 }
 
@@ -70,16 +68,16 @@ function maybe_build_merging_software() {
 function build_CMSSW_7_0_4() {
 	echo "Building CMSSW 7.0.4 project..."
 	initialize_kerberos_keytab
-	initialize_scram_project CMSSW CMSSW_7_0_4
+	initialize_scram_project "CMSSW" "CMSSW_7_0_4"
 	compile_scram_project
-	maybe_build_merging_software $1
+	maybe_build_merging_software "$1"
 }
 
 # Executes given command and logs stdout both to the file and the screen.
 # $1 - commnad to be executed
 function execute_and_log() {
 	echo "Executing command '$1'"
-	eval $1 2>&1 | tee -a ${AGENT_BUILD_LOG}
+	eval "$1" 2>&1 | tee -a "$AGENT_BUILD_LOG"
 }
 
 execute_and_log "build_CMSSW_7_0_4 $1"
