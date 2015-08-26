6568c69297b350f999016009f23cfc14dbb282d5
Tomasz Lichon <tomasz.jan.lichon@cern.ch>
2015-08-26 15:11:18 +0300

    TotemHit refactoring.

--- SimG4CMS/Forward/interface/TotemHit.h
+++ SimG4CMS/Forward/interface/TotemHit.h
@@ -7,52 +7,72 @@ Hit class for analysis in TotemTest
 
 #include<iostream>
 
-class TotemHit{
+class TotemHit {
+public:
+    TotemHit(int, float, float, int, int,
+             int, float, float, float, float,
+             float, float, float, float, float, float, float, float);
 
- public:
+    TotemHit() { }
 
-//  TotemHit(int det,float eloss, float pabs, int pid, int trackid,
-//               int parent,float x, float y, float z,float vx, float vy, float vz);
+    int det() { return _det; }
 
-  TotemHit(int, float, float, int, int,
-               int,float, float, float,float, float, float,float, float, float,float, float, float);
-TotemHit(){}
-virtual ~TotemHit();
-int det() {return _det;}
- int pid() {return _pid;}
- int trackid() {return _trackid;}
- int parent() {return _parent;}
-  float eloss() {return _eloss;}
- float pabs() {return _pabs;}
- float x() {return _x;}
-float y() {return _y;}
-float z() {return _z;}
-float vx() {return _vx;}
-float vy() {return _vy;}
-float vz() {return _vz;}
- float px() {return _px;}
- float py() {return _py;}
- float pz() {return _pz;}
+    int pid() { return _pid; }
 
- float vpx() {return _vpx;}
- float vpy() {return _vpy;}
- float vpz() {return _vpz;}
+    int trackid() { return _trackid; }
 
+    int parent() { return _parent; }
 
- void Print();
+    float eloss() { return _eloss; }
 
- private:
+    float pabs() { return _pabs; }
 
-  int _det, _pid, _trackid, _parent;
-  float _eloss, _pabs, _x, _y, _z, _vx, _vy, _vz, _px,_py,_pz, _vpx,_vpy,_vpz;
+    float x() const { return _x; }
 
+    float y() const { return _y; }
 
+    float z() const { return _z; }
+
+    float vx() { return _vx; }
+
+    float vy() { return _vy; }
+
+    float vz() { return _vz; }
+
+    float px() { return _px; }
+
+    float py() { return _py; }
+
+    float pz() { return _pz; }
+
+    float vpx() { return _vpx; }
+
+    float vpy() { return _vpy; }
+
+    float vpz() { return _vpz; }
+
+    void Print() const;
+
+private:
+    int _det;
+    float _eloss;
+    float _pabs;
+    int _pid;
+    int _trackid;
+    int _parent;
+    float _x;
+    float _y;
+    float _z;
+    float _vx;
+    float _vy;
+    float _vz;
+    float _px;
+    float _py;
+    float _pz;
+    float _vpx;
+    float _vpy;
+    float _vpz;
 
 };
-
-
-
-
-
 
 #endif
