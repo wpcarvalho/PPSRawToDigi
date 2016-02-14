1370743488eda3998de4db82f6dc99fd908f4e99
Sandro Fonseca De Souza <sandro.fonseca@cern.ch>
2016-02-14 12:52:44 -0200

    Modify some config file (all # TODO ) that are missing for 75
    It is open issue that we should discuss with Totem experts.

--- TotemRawDataLibrary/test/test.py
+++ TotemRawDataLibrary/test/test.py
@@ -7,29 +7,38 @@ process.maxEvents = cms.untracked.PSet(
 input = cms.untracked.int32(10)
 )
 
-process.load('TotemRawData.Readers.RawDataSource_cfi')
-process.source.fileNames = cms.untracked.vstring()
+#process.load('TotemRawDataLibrary.Readers.RawDataSource_cfi')
+#process.source.fileNames = cms.untracked.vstring()
 
 # TODO change input files here
 process.source.fileNames.append('root://eostotem//eos/totem/data/rawdata/2015/run_9998_EVB15_1.001.srs')
 
+# Should review these config file
+process.load("Configuration.TotemCommon.LoggerMin_cfi") # cmssw_7_0
+#process.load("Configuration.TotemCommon.geometryGlobal_cfi") # cmssw_7_0
+process.load("Configuration.TotemCommon.geometryRP_PPS_cfi") 
 
-process.load("Configuration.TotemCommon.LoggerMin_cfi")
-
-process.load("Configuration.TotemCommon.geometryGlobal_cfi")
 
 # TODO - check RP distances, check if commited to repository
-process.XMLIdealGeometryESSource.geomXMLFiles.append("Geometry/TotemRPData/data/2015_08_25_fill4269_2/RP_Dist_Beam_Cent.xml")
+#It is not there
+#process.XMLIdealGeometryESSource.geomXMLFiles.append("Geometry/TotemRPData/data/2015_08_25_fill4269_2/RP_Dist_Beam_Cent.xml")
+
+process.XMLIdealGeometryESSource.geomXMLFiles.append("Geometry/TotemRPData/data/2013_02_14/RP_Dist_Beam_Cent.xml")
+
+
 
 # TODO - check RP alignement, check if commited to repository
 process.load("TotemAlignment.RPDataFormats.TotemRPIncludeAlignments_cfi")
-process.TotemRPIncludeAlignments.RealFiles = cms.vstring(
-"TotemAlignment/RPData/alignment_tb_run9509_45_no20.xml",
-"TotemAlignment/RPData/alignment_tb_run9509_56.xml"
-)
+# It is not there
+#process.TotemRPIncludeAlignments.RealFiles = cms.vstring(
+#"TotemAlignment/RPData/alignment_tb_run9509_45_no20.xml",
+#"TotemAlignment/RPData/alignment_tb_run9509_56.xml"
+#)
 
 # TODO - check if optics commited to repository
-process.load("Configuration.TotemOpticsConfiguration.OpticsConfig_6500GeV_19p2_cfi")
+#It is not there
+#process.load("Configuration.TotemOpticsConfiguration.OpticsConfig_6500GeV_19p2_cfi") 
+process.load("Configuration.TotemOpticsConfiguration.OpticsConfig_6500GeV_90_cfi")
 
 process.load('TotemCondFormats.DAQInformation.DAQMappingSourceXML_cfi')
 process.DAQMappingSourceXML.mappingFileNames.append('TotemCondFormats/DAQInformation/data/rp_220_210far.xml')
@@ -50,16 +59,19 @@ process.load("RecoTotemRP.RPClusterizer.RPClusterizationConf_cfi")
 process.RPClustProd.DigiLabel = cms.InputTag("Raw2DigiProducer")
 process.load("RecoTotemRP.RPRecoHitProducer.RPRecoHitProdConf_cfi")
 process.load("RecoTotemRP.RPSingleCandidateTrackFinder.RPSingleTrackCandFindConf_cfi")
-process.load("RecoTotemRP.RPNonParallelTrackCandidateFinder.RPNonParallelTrackCandidateFinder_cfi")
+
+#process.load("RecoTotemRP.RPNonParallelTrackCandidateFinder.RPNonParallelTrackCandidateFinder_cfi")
 process.load("RecoTotemRP.RPTrackCandidateCollectionFitter.RPSingleTrackCandCollFitted_cfi")
 process.RPSingleTrackCandCollFit.RPTrackCandCollProducer = 'NonParallelTrackFinder'
-process.load("RecoTotemRP.RPMulTrackCandidateCollectionFitter.RPMulTrackNonParallelRecoFitter_cfi")
+#process.load("RecoTotemRP.RPMulTrackCandidateCollectionFitter.RPMulTrackNonParallelRecoFitter_cfi")
 
-process.load("L1TriggerTotem.CoincidenceChip.RPCoincidenceProducer_cfi")
-process.RPCC.DetTriggerLabel = cms.InputTag("TriggerBits")
+#process.load("L1TriggerTotem.CoincidenceChip.RPCoincidenceProducer_cfi")
+#process.RPCC.DetTriggerLabel = cms.InputTag("TriggerBits")
 
 # TODO - check if reconstruction settings commited to repository
-process.load("RecoTotemRP.RPInelasticReconstruction.RPRec220_19p2_cfi")
+# it is not there
+#process.load("RecoTotemRP.RPInelasticReconstruction.RPRec220_19p2_cfi")
+process.load("RecoTotemRP.RPInelasticReconstruction.RPRec220_cfi")
 process.RP220Reconst.BeamProtTransportSetup = process.BeamProtTransportSetup
 
 process.output = cms.OutputModule(
@@ -74,10 +86,10 @@ process.Raw2DigiProducer
 *process.RPClustProd
 *process.RPHecoHitProd
 *process.RPSinglTrackCandFind
-*process.NonParallelTrackFinder
+#*process.NonParallelTrackFinder
 *process.RPSingleTrackCandCollFit
-*process.RPMulTrackNonParallelCandCollFit
-*process.RPCC
+#*process.RPMulTrackNonParallelCandCollFit
+#*process.RPCC
 *process.RP220Reconst
 )
 
