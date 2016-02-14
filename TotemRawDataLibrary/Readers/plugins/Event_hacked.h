4139d645c9d2d183e1c835531abc5fed809ed43b
Sandro Fonseca De Souza <sandro.fonseca@cern.ch>
2016-02-14 12:42:54 -0200

    WrapperOwningHolder.h does not exist on CMSSW_8_0
    Found a another bug fix for Event_hacked but I fix it up to re-write
    for CMSSW_8_0_X:
    
    /afs/cern.ch/work/s/sfonseca/PPS/CT-PPSTOTEM_CMSSW_7_5_X/testing_raw_dat
    a_files/CMSSW_8_0_0_pre5/src/TotemRawDataLibrary/Readers/plugins/Event_h
    acked.h:28:62: fatal error:
    DataFormats/Common/interface/WrapperOwningHolder.h: No such file or
    directory
     #include "DataFormats/Common/interface/WrapperOwningHolder.h"
                                                                  ^
    compilation terminated.
    In file included from
    /afs/cern.ch/work/s/sfonseca/PPS/CT-PPSTOTEM_CMSSW_7_5_X/testing_raw_dat
    a_files/CMSSW_8_0_0_pre5/src/TotemRawDataLibrary/Readers/plugins/RawData
    Source.cc:10:0:
    /afs/cern.ch/work/s/sfonseca/PPS/CT-PPSTOTEM_CMSSW_7_5_X/testing_raw_dat
    a_files/CMSSW_8_0_0_pre5/src/TotemRawDataLibrary/Readers/plugins/Event_h
    acked.h:28:62: fatal error:
    DataFormats/Common/interface/WrapperOwningHolder.h: No such file or
    directory
     #include "DataFormats/Common/interface/WrapperOwningHolder.h"
    
    I

--- TotemRawDataLibrary/Readers/plugins/Event_hacked.h
+++ TotemRawDataLibrary/Readers/plugins/Event_hacked.h
@@ -1,13 +1,12 @@
 #ifndef FWCore_Framework_Event_h
 #define FWCore_Framework_Event_h
 
-/**
- * This is a copy of
- *   src/FWCore/Framework/interface/Event.h
- * with RawDataSource added as a friend.
- */
-
 // -*- C++ -*-
+///**
+//<  * This is a copy of
+//<  *   src/FWCore/Framework/interface/Event.h
+//<  * with RawDataSource added as a friend.
+//<  */
 //
 // Package:     Framework
 // Class  :     Event
@@ -25,10 +24,10 @@ For its usage, see "FWCore/Framework/interface/PrincipalGetAdapter.h"
 
 #include "DataFormats/Common/interface/BasicHandle.h"
 #include "DataFormats/Common/interface/ConvertHandle.h"
-#include "DataFormats/Common/interface/WrapperOwningHolder.h"
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Common/interface/OrphanHandle.h"
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/FillViewHelperVector.h"
 #include "DataFormats/Common/interface/FunctorHandleExceptionFactory.h"
 
 #include "DataFormats/Provenance/interface/EventID.h"
@@ -44,16 +43,16 @@ For its usage, see "FWCore/Framework/interface/PrincipalGetAdapter.h"
 #include "FWCore/Utilities/interface/ProductKindOfType.h"
 #include "FWCore/Utilities/interface/StreamID.h"
 
-#include "boost/shared_ptr.hpp"
-
 #include <memory>
 #include <string>
 #include <set>
 #include <typeinfo>
 #include <vector>
 
-class RawDataSource;
+//#include "boost/shared_ptr.hpp"
 
+//class testEventGetRefBeforePut;
+class RawDataSource;
 namespace edm {
 
   class BranchDescription;
@@ -62,19 +61,23 @@ namespace edm {
   class TriggerResults;
   class TriggerNames;
   class EDConsumerBase;
+  class EDProductGetter;
   class ProducerBase;
+  class SharedResourcesAcquirer;
   namespace stream {
     template< typename T> class ProducingModuleAdaptorBase;
   }
 
   class Event : public EventBase {
   public:
-    Event(EventPrincipal& ep, ModuleDescription const& md,
+    Event(EventPrincipal const& ep, ModuleDescription const& md,
           ModuleCallingContext const*);
     virtual ~Event();
     
     //Used in conjunction with EDGetToken
     void setConsumer(EDConsumerBase const* iConsumer);
+    
+    void setSharedResourcesAcquirer( SharedResourcesAcquirer* iResourceAcquirer);
     
     // AUX functions are defined in EventBase
     EventAuxiliary const& eventAuxiliary() const {return aux_;}
@@ -123,10 +126,18 @@ namespace edm {
     OrphanHandle<PROD>
     put(std::auto_ptr<PROD> product) {return put<PROD>(product, std::string());}
 
+    template<typename PROD>
+    OrphanHandle<PROD>
+    put(std::unique_ptr<PROD> product) {return put<PROD>(std::move(product), std::string());}
+
     ///Put a new product with a 'product instance name'
     template<typename PROD>
     OrphanHandle<PROD>
     put(std::auto_ptr<PROD> product, std::string const& productInstanceName);
+
+    template<typename PROD>
+    OrphanHandle<PROD>
+    put(std::unique_ptr<PROD> product, std::string const& productInstanceName);
 
     ///Returns a RefProd to a product before that product has been placed into the Event.
     /// The RefProd (and any Ref's made from it) will no work properly until after the
@@ -219,20 +230,28 @@ namespace edm {
 
     ModuleCallingContext const* moduleCallingContext() const { return moduleCallingContext_; }
 
-    typedef std::vector<std::pair<WrapperOwningHolder, BranchDescription const*> > ProductPtrVec;
+    void labelsForToken(EDGetToken const& iToken, ProductLabels& oLabels) const { provRecorder_.labelsForToken(iToken, oLabels); }
+
+    typedef std::vector<std::pair<std::unique_ptr<WrapperBase>, BranchDescription const*> > ProductPtrVec;
+
+    EDProductGetter const&
+    productGetter() const;
 
   private:
+    //for testing
+    //friend class ::testEventGetRefBeforePut;
+    
     EventPrincipal const&
     eventPrincipal() const;
-
-    EventPrincipal&
-    eventPrincipal();
 
     ProductID
     makeProductID(BranchDescription const& desc) const;
 
     //override used by EventBase class
     virtual BasicHandle getByLabelImpl(std::type_info const& iWrapperType, std::type_info const& iProductType, InputTag const& iTag) const;
+
+    //override used by EventBase class
+    virtual BasicHandle getImpl(std::type_info const& iProductType, ProductID const& pid) const;
 
     // commit_() is called to complete the transaction represented by
     // this PrincipalGetAdapter. The friendships required seems gross, but any
@@ -242,7 +261,7 @@ namespace edm {
     friend class InputSource;
     friend class RawInputSource;
     friend class ProducerBase;
-    friend class ::RawDataSource;
+    friend class ::RawDataSource;  //Include for CT-PPS
     template<typename T> friend class stream::ProducingModuleAdaptorBase;
 
     void commit_(std::vector<BranchID>* previousParentage= 0, ParentageID* previousParentageId = 0);
@@ -255,6 +274,7 @@ namespace edm {
     ProductPtrVec const& putProducts() const {return putProducts_;}
 
     ProductPtrVec& putProductsWithoutParents() {return putProductsWithoutParents_;}
+
     ProductPtrVec const& putProductsWithoutParents() const {return putProductsWithoutParents_;}
 
     PrincipalGetAdapter provRecorder_;
@@ -267,7 +287,7 @@ namespace edm {
     ProductPtrVec putProductsWithoutParents_; // ... but not for these
 
     EventAuxiliary const& aux_;
-    boost::shared_ptr<LuminosityBlock const> const luminosityBlock_;
+    std::shared_ptr<LuminosityBlock const> const luminosityBlock_;
 
     // gotBranchIDs_ must be mutable because it records all 'gets',
     // which do not logically modify the PrincipalGetAdapter. gotBranchIDs_ is
@@ -278,7 +298,7 @@ namespace edm {
     void addToGotBranchIDs(Provenance const& prov) const;
 
     // We own the retrieved Views, and have to destroy them.
-    mutable std::vector<boost::shared_ptr<ViewBase> > gotViews_;
+    mutable std::vector<std::shared_ptr<ViewBase> > gotViews_;
     
     StreamID streamID_;
     ModuleCallingContext const* moduleCallingContext_;
@@ -294,9 +314,9 @@ namespace edm {
     typedef Event::ProductPtrVec ptrvec_t;
     void do_it(ptrvec_t& /*ignored*/,
                ptrvec_t& used,
-               WrapperOwningHolder const& edp,
+               std::unique_ptr<Wrapper<PROD> > wp,
                BranchDescription const* desc) const {
-      used.emplace_back(edp, desc);
+      used.emplace_back(std::move(wp), desc);
     }
   };
 
@@ -306,9 +326,9 @@ namespace edm {
 
     void do_it(ptrvec_t& used,
                ptrvec_t& /*ignored*/,
-               WrapperOwningHolder const& edp,
+               std::unique_ptr<Wrapper<PROD> > wp,
                BranchDescription const* desc) const {
-      used.emplace_back(edp, desc);
+      used.emplace_back(std::move(wp), desc);
     }
   };
 
@@ -334,7 +354,7 @@ namespace edm {
 
       if(bh.failedToGet()) {
           Handle<View<ELEMENT> > temp(makeHandleExceptionFactory([oid]()->std::shared_ptr<cms::Exception> {
-            std::shared_ptr<cms::Exception> whyFailed(new edm::Exception(edm::errors::ProductNotFound));
+            std::shared_ptr<cms::Exception> whyFailed = std::make_shared<edm::Exception>(edm::errors::ProductNotFound);
             *whyFailed
             << "get View by ID failed: no product with ID = " << oid <<"\n";
             return whyFailed;
@@ -350,6 +370,11 @@ namespace edm {
   template<typename PROD>
   OrphanHandle<PROD>
   Event::put(std::auto_ptr<PROD> product, std::string const& productInstanceName) {
+    return put(std::unique_ptr<PROD>(product.release()),productInstanceName);
+  }
+  template<typename PROD>
+  OrphanHandle<PROD>
+  Event::put(std::unique_ptr<PROD> product, std::string const& productInstanceName) {
     if(product.get() == 0) {                // null pointer is illegal
       TypeID typeID(typeid(PROD));
       principal_get_adapter_detail::throwOnPutOfNullProduct("Event", typeID, productInstanceName);
@@ -365,14 +390,15 @@ namespace edm {
     BranchDescription const& desc =
       provRecorder_.getBranchDescription(TypeID(*product), productInstanceName);
 
-    WrapperOwningHolder edp(new Wrapper<PROD>(product), Wrapper<PROD>::getInterface());
-
+    std::unique_ptr<Wrapper<PROD> > wp(new Wrapper<PROD>(std::move(product)));
+    PROD const* prod = wp->product(); 
+     
     typename boost::mpl::if_c<detail::has_donotrecordparents<PROD>::value,
       RecordInParentless<PROD>,
       RecordInParentfull<PROD> >::type parentage_recorder;
     parentage_recorder.do_it(putProducts(),
                              putProductsWithoutParents(),
-                             edp,
+                             std::move(wp),
                              &desc);
 
     //  putProducts().push_back(std::make_pair(edp, &desc));
@@ -380,7 +406,7 @@ namespace edm {
     // product.release(); // The object has been copied into the Wrapper.
     // The old copy must be deleted, so we cannot release ownership.
 
-    return(OrphanHandle<PROD>(static_cast<Wrapper<PROD> const*>(edp.wrapper())->product(), makeProductID(desc)));
+    return(OrphanHandle<PROD>(prod, makeProductID(desc)));
   }
 
   template<typename PROD>
@@ -534,15 +560,12 @@ namespace edm {
   void
   Event::fillView_(BasicHandle& bh, Handle<View<ELEMENT> >& result) const {
     std::vector<void const*> pointersToElements;
-    // the following is a shared pointer.
-    // It is not initialized here
-    helper_vector_ptr helpers;
+    FillViewHelperVector helpers;
     // the following must initialize the
-    //  shared pointer and fill the helper vector
-    bh.interface()->fillView(bh.wrapper(), bh.id(), pointersToElements, helpers);
+    //  fill the helper vector
+    bh.wrapper()->fillView(bh.id(), pointersToElements, helpers);
 
-    boost::shared_ptr<View<ELEMENT> >
-      newview(new View<ELEMENT>(pointersToElements, helpers));
+    auto newview = std::make_shared<View<ELEMENT> >(pointersToElements, helpers, &(productGetter()));
 
     addToGotBranchIDs(*bh.provenance());
     gotViews_.push_back(newview);
@@ -551,3 +574,4 @@ namespace edm {
   }
 }
 #endif
+
