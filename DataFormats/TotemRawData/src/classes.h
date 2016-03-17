b0d3e63c4ea9db3ac64f5d9bcebc4f299556b711
Jan Kašpar <jan.kaspar@cern.ch>
2016-03-17 18:25:25 +0100

    Added TotemRawToDigiStatus class.

--- DataFormats/TotemRawData/src/classes.h
+++ DataFormats/TotemRawData/src/classes.h
@@ -8,10 +8,21 @@
 ****************************************************************************/
 
 #include "DataFormats/TotemRawData/interface/TotemRawEvent.h"
+#include "DataFormats/TotemRawData/interface/TotemRawToDigiStatus.h"
 
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace { namespace {
 	TotemRawEvent dummy0;
 	edm::Wrapper<TotemRawEvent> dummy0_2;
+
+	Raw2DigiStatus dummy11;
+	edm::Wrapper< Raw2DigiStatus > dummy12;
+
+	VFATStatus dummy13;
+	edm::Wrapper< VFATStatus > dummy14;
+
+	// TODO: needed ??
+	std::bitset<8> dummy15;
+	edm::Wrapper< std::bitset<8> > dummy16;
 }}
