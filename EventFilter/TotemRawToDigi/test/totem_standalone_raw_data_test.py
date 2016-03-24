099e19331cc854a8f8f350f54990c489ffe9bd07
Jan Kašpar <jan.kaspar@cern.ch>
2016-03-24 12:54:48 +0100

    Added a test executable by anyone and with the expected output.

--- EventFilter/TotemRawToDigi/test/totem_standalone_raw_data_test.py
+++ EventFilter/TotemRawToDigi/test/totem_standalone_raw_data_test.py
@@ -3,22 +3,27 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("TotemStandaloneRawDataTest")
 
 # minimum of logs
-process.load("Configuration.TotemCommon.LoggerMin_cfi")
-
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(3)
+process.MessageLogger = cms.Service("MessageLogger",
+    statistics = cms.untracked.vstring(),
+    destinations = cms.untracked.vstring('cerr'),
+    cerr = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING')
+    )
 )
 
+# raw data source
 process.load('EventFilter.TotemRawToDigi.TotemStandaloneRawDataSource_cfi')
 process.source.verbosity = 10
 process.source.printProgressFrequency = 0
-# eos cp /eos/totem/data/rawdata/2015/run_9998_EVB11_1.000.srs .
-process.source.fileNames.append('run_9998_EVB11_1.000.srs')
+process.source.fileNames.append('/afs/cern.ch/user/j/jkaspar/public/run_9987_EVB11_1.003.srs')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
 
 # raw to digi conversion
 process.load('CondFormats.TotemReadoutObjects.TotemDAQMappingESSourceXML_cfi')
-process.TotemDAQMappingESSourceXML.mappingFileNames.append("CondFormats/TotemReadoutObjects/xml/totem_rp_210_mapping.xml")
-process.TotemDAQMappingESSourceXML.mappingFileNames.append("CondFormats/TotemReadoutObjects/xml/totem_rp_220_mapping.xml")
+process.TotemDAQMappingESSourceXML.mappingFileNames.append("CondFormats/TotemReadoutObjects/xml/totem_rp_210far_220_mapping.xml")
 
 process.load('EventFilter.TotemRawToDigi.TotemRawToDigi_cfi')
 process.TotemRawToDigi.rawDataTag = cms.InputTag("source")
