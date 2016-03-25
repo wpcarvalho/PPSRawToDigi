44f8105b1a0e066e2b075231c837ca9aaa5f29cc
Leszek Grzanka <Leszek.Grzanka@ifj.edu.pl>
2016-03-25 11:03:23 +0100

    synchronized with CMSSW 8_1_0 code

--- Geometry/CMSCommonData/python/cmsMagneticFieldXML_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-#Temporary workaround, should be removed!
-# from Configuration.StandardSequences.MagneticField_30T_cff import *
-# from Configuration.StandardSequences.MagneticField_35T_cff import *
-from Configuration.StandardSequences.MagneticField_38T_cff import *
-# from Configuration.StandardSequences.MagneticField_40T_cff import *
-
