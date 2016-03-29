f72b9a1ac9759c71b365139d907748f8d566f1b5
Jan Kašpar <jan.kaspar@cern.ch>
2016-03-29 13:57:15 +0200

    Added versions and checksums to classes_def.xml.

--- DataFormats/TotemRawData/interface/TotemRawEvent.h
+++ DataFormats/TotemRawData/interface/TotemRawEvent.h
@@ -17,24 +17,16 @@
 **/
 class TotemRawEvent
 {
-  public:
-    /// number of event in the datafile (0xb tag)
-    unsigned long dataEventNumber;
-   
-    /// number of configuration in the datafile (0xb tag)
-    unsigned long dataConfNumber;
-    
-    /// timestamp
-    time_t timestamp;
+  public:  
+    TotemRawEvent() {}
+
+    ~TotemRawEvent() {}
 
     ///Additional data provided in OptoRx headers and footers.
     struct OptoRxMetaData
     {
       unsigned int BX, LV1;
     };
-
-    /// additional data from OptoRx frames, indexed by OptoRxId
-    std::map<unsigned int, OptoRxMetaData> optoRxMetaData;
 
     /// Trigger data provided by LoneG.
     struct TriggerData
@@ -46,15 +38,24 @@ class TotemRawEvent
       unsigned int run_num, trigger_num, inhibited_triggers_num, input_status_bits;
     };
 
+  public:
+    /// number of event in the datafile (0xb tag)
+    unsigned long dataEventNumber;
+   
+    /// number of configuration in the datafile (0xb tag)
+    unsigned long dataConfNumber;
+    
+    /// timestamp
+    time_t timestamp;
+
+    /// additional data from OptoRx frames, indexed by OptoRxId
+    std::map<unsigned int, OptoRxMetaData> optoRxMetaData;
+
     /// trigger (LoneG) data
     TriggerData triggerData;
 
     /// map: LDC id -> LDC timestamp
     std::map<unsigned int, time_t> ldcTimeStamps;
-    
-    TotemRawEvent();
-
-    ~TotemRawEvent();
 };
 
 #endif
