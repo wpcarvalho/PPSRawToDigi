6d8cd9c8e8cdf14981e6c0793aa521bc749bb4f6
Jan Kaspar <jan.kaspar@cern.ch>
2016-04-07 09:59:54 +0200

    Testing the first data from integrated DAQ.

--- EventFilter/TotemRawToDigi/test/totem_integrated_raw_data_test.py
+++ EventFilter/TotemRawToDigi/test/totem_integrated_raw_data_test.py
@@ -12,10 +12,12 @@ process.MessageLogger = cms.Service("MessageLogger",
 )
 
 # raw data source
-# TODO: put here the CMS raw-data source module
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/j/jkaspar/public/run268608_ls0001_streamA_StorageManager.root')
+)
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)  # TODO: number of events to process
+    input = cms.untracked.int32(10)
 )
 
 # raw to digi conversion
@@ -23,8 +25,9 @@ process.load('CondFormats.TotemReadoutObjects.TotemDAQMappingESSourceXML_cfi')
 process.TotemDAQMappingESSourceXML.mappingFileNames.append("CondFormats/TotemReadoutObjects/xml/totem_rp_210far_220_mapping.xml")
 
 process.load('EventFilter.TotemRawToDigi.TotemRawToDigi_cfi')
-process.TotemRawToDigi.rawDataTag = cms.InputTag("source")  # TODO: this may need tuning
-process.TotemRawToDigi.fedIds = cms.vuint32(577, 578, 579, 580, 581)  # TODO: this may need tuning
+process.TotemRawToDigi.rawDataTag = cms.InputTag("rawDataCollector")
+#process.TotemRawToDigi.fedIds = cms.vuint32(577, 578, 579, 580, 581)
+process.TotemRawToDigi.fedIds = cms.vuint32(577, 578, 579, 580)
 process.TotemRawToDigi.RawToDigi.printErrorSummary = 1
 process.TotemRawToDigi.RawToDigi.printUnknownFrameSummary = 1
 
