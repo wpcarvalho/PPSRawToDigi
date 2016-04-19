f88d999578bccab79ae529b31a1ab89b9f9ef885
Leszek Grzanka <Leszek.Grzanka@ifj.edu.pl>
2016-04-19 11:40:42 +0200

    updated test script for travis

--- test.sh
+++ test.sh
@@ -11,4 +11,7 @@ shopt -s expand_aliases # Expand command alias to the command itself.
 source /opt/cms/cmsset_default.sh
 scram b
 eval `scram runtime -sh`
-cmsRun raw_data_chain_test.py
+sed -i 's#/afs/cern.ch/user/j/jkaspar/public/run_9987_EVB11_1.003#sample#g' raw_data_chain_test.py
+sed -i 's#/afs/cern.ch/user/j/jkaspar/public/run_9987_EVB11_1.003#sample#g' raw_data_chain_test.reference_output
+cmsRun raw_data_chain_test.py > raw_data_chain_test.output
+diff raw_data_chain_test.output raw_data_chain_test.reference_output
