1e3843d673dbb2bb37865f3d712d9c767cde29de
Jan Kašpar <jan.kaspar@cern.ch>
2016-04-19 17:33:01 +0200

    CondFormats/DataRecord synchronised with branch 8_1_X.

---
+++ CondFormats/DataRecord/interface/CSCIdentifierRcd.h
@@ -0,0 +1,6 @@
+#ifndef CSCIDENTIFIERRCD_H
+#define CSCIDENTIFIERRCD_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class CSCIdentifierRcd : public edm::eventsetup::EventSetupRecordImplementation<CSCIdentifierRcd> {};
+#endif // CSCIDENTIFIERRCD_H
