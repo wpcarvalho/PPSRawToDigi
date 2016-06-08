471ecf1a5b45ac77ca8c0c0cbb04eba00d3a47bb
Jan Kašpar <jan.kaspar@cern.ch>
2016-06-08 13:50:13 +0200

    Efficiency plots finished.

--- DQM/CTPPS/plugins/TotemRPDQMHarvester.cc
+++ DQM/CTPPS/plugins/TotemRPDQMHarvester.cc
@@ -28,7 +28,8 @@ class TotemRPDQMHarvester: public DQMEDHarvester
     void dqmEndJob(DQMStore::IBooker &, DQMStore::IGetter &) override;
 
   private:
-    void MakePlaneEfficiencyHistograms(unsigned int plId, DQMStore::IBooker& ibooker, DQMStore::IGetter& igetter);
+    void MakePlaneEfficiencyHistograms(unsigned int plId, DQMStore::IBooker& ibooker, DQMStore::IGetter& igetter,
+      MonitorElement* &rp_efficiency);
 };
 
 //----------------------------------------------------------------------------------------------------
@@ -51,29 +52,43 @@ TotemRPDQMHarvester::~TotemRPDQMHarvester()
 
 //----------------------------------------------------------------------------------------------------
 
-void TotemRPDQMHarvester::MakePlaneEfficiencyHistograms(unsigned int id, DQMStore::IBooker& ibooker, DQMStore::IGetter& igetter)
+void TotemRPDQMHarvester::MakePlaneEfficiencyHistograms(unsigned int id, DQMStore::IBooker& ibooker, DQMStore::IGetter& igetter,
+  MonitorElement* &rp_efficiency)
 {
   printf(">> TotemRPDQMHarvester::MakePlaneEfficiencyHistograms(%u)\n", id);
 
+  // get source histograms
   string path = TotemRPDetId::planeName(id, TotemRPDetId::nPath);
   path.replace(0, 2, "TrackingStrip");
-  ibooker.setCurrentFolder(string("CTPPS/") + path);
 
-
-  printf("    num: %s\n", ("CTPPS/" + path + "/efficiency_num").c_str());
-
-  MonitorElement *efficiency_num = igetter.get("CTPPS/" + path + "/efficiency_num");
-  MonitorElement *efficiency_den = igetter.get("CTPPS/" + path + "/efficiency_den");
-
-  printf("    num = %p, den = %p\n", efficiency_num, efficiency_den);
+  MonitorElement *efficiency_num = igetter.get("CTPPS/" + path + "/efficiency num");
+  MonitorElement *efficiency_den = igetter.get("CTPPS/" + path + "/efficiency den");
 
   if (!efficiency_num || !efficiency_den)
     return;
-  
+
+  // book new histogram
+  ibooker.setCurrentFolder(string("CTPPS/") + path);
+
   string title = TotemRPDetId::planeName(id, TotemRPDetId::nFull);
   
-  MonitorElement *efficiency = ibooker.book1D("efficiency", title+";track position   (mm)", 30, -15., 0.);
+  TAxis *axis = efficiency_den->getTH1()->GetXaxis();
+  MonitorElement *efficiency = ibooker.book1D("efficiency", title+";track position   (mm)", axis->GetNbins(), axis->GetXmin(), axis->GetXmax());
 
+  // book new RP histogram (if not yet done)
+  if (rp_efficiency == NULL)
+  {
+    printf("    new RP hist, %i\n", id);
+
+    path = TotemRPDetId::rpName(id/10, TotemRPDetId::nPath);
+    path.replace(0, 2, "TrackingStrip");
+    title = TotemRPDetId::rpName(id/10, TotemRPDetId::nFull);
+    ibooker.setCurrentFolder(string("CTPPS/") + path);
+    rp_efficiency = ibooker.book2D("plane efficiency", title+";plane;track position   (mm)",
+      10, -0.5, 9.5, axis->GetNbins(), axis->GetXmin(), axis->GetXmax());
+  }
+
+  // calculate and fill efficiencies
   for (signed int bi = 1; bi <= efficiency->getNbinsX(); bi++)
   {
     double num = efficiency_num->getBinContent(bi);
@@ -82,9 +97,14 @@ void TotemRPDQMHarvester::MakePlaneEfficiencyHistograms(unsigned int id, DQMStor
     if (den > 0)
     {
       double p = num / den;
-      double p_unc = sqrt(den * p * (1. - p));
+      double p_unc = sqrt(p * (1. - p) / den);
       efficiency->setBinContent(bi, p);
-      efficiency->setBinError(bi, p);
+      efficiency->setBinError(bi, p_unc);
+
+      int pl_bi = (id%10) + 1;
+      rp_efficiency->setBinContent(pl_bi, bi, p);
+
+      printf("        %i, %i, %.2f\n", pl_bi, bi, p);
     } else {
       efficiency->setBinContent(bi, 0.);
     }
@@ -108,12 +128,14 @@ void TotemRPDQMHarvester::dqmEndJob(DQMStore::IBooker& ibooker, DQMStore::IGette
       {
         unsigned int rpId = 10*stId + rp;
 
+        MonitorElement *rp_efficiency = NULL;
+
         // loop over planes
         for (unsigned int pl = 0; pl < 10; ++pl)
         {
           unsigned int plId = 10*rpId + pl;
 
-          MakePlaneEfficiencyHistograms(plId, ibooker, igetter);
+          MakePlaneEfficiencyHistograms(plId, ibooker, igetter, rp_efficiency);
         }
       }
     }
