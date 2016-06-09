1e461d54189d69fbe1da4dceb456d4b5e5feeec7
Jan Kašpar <jan.kaspar@cern.ch>
2016-06-09 18:08:07 +0200

    Updated config for online DQM.

---
+++ DQM/Integration/python/clients/ctpps_dqm_sourceclient-live_cfg.py
@@ -0,0 +1,75 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("DQM")
+
+test = False
+
+# event source
+if not test:
+  # for live online DQM in P5
+  process.load("DQM.Integration.config.inputsource_cfi")
+else:
+  # for testing in lxplus
+  process.load("DQM.Integration.config.fileinputsource_cfi")
+  process.source.fileNames = cms.untracked.vstring(
+    "'file:///afs/cern.ch/user/j/jkaspar/public/run273062_ls0001-2_stream.root"
+  )
+
+
+# DQM environment
+process.load("DQM.Integration.config.environment_cfi")
+process.dqmEnv.subSystemFolder = 'Totem'
+process.dqmSaver.tag = 'Totem'
+
+if test:
+  process.dqmSaver.path = "."
+
+process.load("DQMServices.Components.DQMProvInfo_cfi")
+
+# message logger
+process.MessageLogger = cms.Service("MessageLogger",
+  destinations = cms.untracked.vstring('cout'),
+  cout = cms.untracked.PSet(threshold = cms.untracked.string('WARNING'))
+)
+
+# global tag - conditions for P5 cluster
+process.load("DQM.Integration.config.FrontierCondition_GT_cfi")
+
+# raw-to-digi conversion
+process.load("EventFilter.TotemRawToDigi.totemRawToDigi_cff")
+
+# local RP reconstruction chain with standard settings
+process.load("RecoCTPPS.Configuration.recoCTPPS_cff")
+
+# DQM Modules
+process.load("DQM.CTPPS.totemDAQTriggerDQMSource_cfi")
+process.load("DQM.CTPPS.totemRPDQMSource_cfi")
+process.load("DQM.CTPPS.totemRPDQMHarvester_cfi")
+
+# processing path
+process.recoStep = cms.Sequence(
+  process.totemTriggerRawToDigi *
+  process.totemRPRawToDigi *
+  process.recoCTPPS
+)
+
+process.dqmModules = cms.Sequence(
+  process.totemDAQTriggerDQMSource +
+  process.totemRPDQMSource +
+  process.totemRPDQMHarvester +
+  process.dqmEnv +
+  process.dqmSaver
+)
+
+process.path = cms.Path(
+  process.recoStep *
+  process.dqmModules
+)
+
+process.schedule = cms.Schedule(process.path)
+
+process.dqmProvInfo.runType = process.runType.getRunTypeName()
+
+# Process customizations included here
+from DQM.Integration.config.online_customizations_cfi import *
+process = customise(process)
