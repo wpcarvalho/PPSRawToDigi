11232756c6acb33bb674cbab0b912b9cb8dd9455
SeyedMohsen <setesami@cern.ch>
2016-07-03 09:01:11 +0200

    add new readout record for daiamond

---
+++ CondFormats/DataRecord/interface/DiamondReadoutRcd.h
@@ -0,0 +1,11 @@
+#ifndef CondFormats_DataRecord_DiamondReadoutRcd
+#define CondFormats_DataRecord_DiamondReadoutRcd
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+ 
+/**
+ *\brief EventSetup record for TOTEM readout-related information.
+ **/
+class DiamondReadoutRcd : public edm::eventsetup::EventSetupRecordImplementation<DiamondReadoutRcd> {};
+ 
+#endif
---
+++ CondFormats/DataRecord/src/DiamondReadoutRcd.cc
@@ -0,0 +1,8 @@
+/****************************************************************************
+* Seyed Mohsen Etesami
+****************************************************************************/
+
+#include "CondFormats/DataRecord/interface/DiamondReadoutRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+ 
+EVENTSETUP_RECORD_REG(DiamondReadoutRcd);
